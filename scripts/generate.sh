6f1e526857b4160ca02bd4da4475cea9dd36f668
Xurvan <xurvan@tutanota.com>
2025-08-13 15:49:07 +0400

    Add cache configs and support env file for migrate command

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -2,7 +2,7 @@
 
 mkdir -p internal/gen/db internal/gen/oapi pkg/gen
 
-oapi-codegen -config oapi-codegen-server.yml ./api/openapi.yml
-oapi-codegen -config oapi-codegen-client.yml ./api/openapi.yml
+oapi-codegen -config ./configs/oapi-codegen-server.yml ./api/openapi.yml
+oapi-codegen -config ./configs/oapi-codegen-client.yml ./api/openapi.yml
 
-sqlc generate -f sqlc.yml
+sqlc generate -f ./configs/sqlc.yml
