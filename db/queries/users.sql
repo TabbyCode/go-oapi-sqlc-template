d662a50539e1be4b3bd51b89057dc6818cbdb999
Xurvan <xurvan@tutanota.com>
2025-08-18 08:55:18 +0400

    Format SQL files with "sqruff"

--- db/queries/users.sql
+++ db/queries/users.sql
@@ -1,29 +1,42 @@
 -- name: GetUser :one
-SELECT * FROM users
-WHERE id = $1 LIMIT 1;
+SELECT
+    id,
+    name,
+    email,
+    created_at,
+    updated_at
+FROM users
+WHERE id = $1
+LIMIT 1;
 
 -- name: ListUsers :many
-SELECT * FROM users
+SELECT
+    id,
+    name,
+    email,
+    created_at,
+    updated_at
+FROM users
 ORDER BY id;
 
 -- name: CreateUser :one
 INSERT INTO users (
-  name,
-  email
-) VALUES (
-  $1, $2
+    name,
+    email
 )
+VALUES ($1, $2)
 RETURNING *;
 
 -- name: UpdateUser :one
 UPDATE users
-SET 
-  name = COALESCE($2, name),
-  email = COALESCE($3, email),
-  updated_at = NOW()
+SET
+    name = COALESCE($2, name),
+    email = COALESCE($3, email),
+    updated_at = NOW()
 WHERE id = $1
 RETURNING *;
 
 -- name: DeleteUser :exec
-DELETE FROM users
+DELETE
+FROM users
 WHERE id = $1;
