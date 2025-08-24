3eeac722b96f8b40cff7ca457d346f18fa4d508b
Xurvan <xurvan@tutanota.com>
2025-08-24 19:28:01 +0400

    Move Fiber init and server start to server module

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,16 +3,8 @@ package main
 
 import (
 	"log"
-	"os"
-	"os/signal"
-	"syscall"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/gofiber/fiber/v2/middleware/recover"
 
 	"github.com/xurvan/go-oapi-sqlc-template/internal/config"
-	"github.com/xurvan/go-oapi-sqlc-template/internal/gen/oapi"
 	"github.com/xurvan/go-oapi-sqlc-template/internal/repository"
 	"github.com/xurvan/go-oapi-sqlc-template/internal/server"
 )
@@ -25,44 +17,6 @@ func main() {
 		log.Fatalf("Failed to connect to database: %s", err)
 	}
 
-	srv := server.NewServer(repo)
-	strict := oapi.NewStrictHandler(srv, nil)
-	app := fiber.New(fiber.Config{
-		ReadTimeout:  cfg.ReadTimeout,
-		WriteTimeout: cfg.WriteTimeout,
-		IdleTimeout:  cfg.IdleTimeout,
-	})
-
-	app.Use(logger.New())
-	app.Use(recover.New())
-
-	oapi.RegisterHandlers(app, strict)
-
-	// Channel to listen for errors coming from the listener
-	serverErrors := make(chan error, 1)
-
-	// Start the server
-	go func() {
-		log.Printf("Starting server on %s", cfg.ListenAddress)
-
-		serverErrors <- app.Listen(cfg.ListenAddress)
-	}()
-
-	// Channel to listen for an interrupt or terminate signal from the OS
-	shutdown := make(chan os.Signal, 1)
-	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
-
-	// Blocking main and waiting for shutdown or server errors
-	select {
-	case err = <-serverErrors:
-		log.Fatalf("Error starting server: %v", err)
-
-	case <-shutdown:
-		err = app.ShutdownWithTimeout(cfg.ShutdownTimeout)
-		if err != nil {
-			log.Printf("Error during server shutdown: %v", err)
-		}
-
-		log.Println("Server gracefully stopped")
-	}
+	srv := server.NewServer(cfg, repo)
+	srv.Start()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -6,8 +6,17 @@ package server
 import (
 	"context"
 	"errors"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+
+	"github.com/xurvan/go-oapi-sqlc-template/internal/config"
 	"github.com/xurvan/go-oapi-sqlc-template/internal/gen/oapi"
 	"github.com/xurvan/go-oapi-sqlc-template/internal/repository"
 )
@@ -15,15 +24,63 @@ import (
 // Server handles HTTP requests for user operations.
 type Server struct {
 	repo *repository.UserRepository
+	app  *fiber.App
+	cfg  *config.Config
 }
 
 // Ensure that Server implements the StrictServerInterface interface at compile time.
 var _ oapi.StrictServerInterface = (*Server)(nil)
 
 // NewServer creates a new Server instance with the given repository.
-func NewServer(db *repository.UserRepository) *Server {
-	return &Server{
-		repo: db,
+func NewServer(cfg *config.Config, repo *repository.UserRepository) *Server {
+	app := fiber.New(fiber.Config{
+		ReadTimeout:  cfg.ReadTimeout,
+		WriteTimeout: cfg.WriteTimeout,
+		IdleTimeout:  cfg.IdleTimeout,
+	})
+	app.Use(logger.New())
+	app.Use(recover.New())
+
+	srv := &Server{
+		repo: repo,
+		app:  app,
+		cfg:  cfg,
+	}
+
+	strict := oapi.NewStrictHandler(srv, nil)
+
+	oapi.RegisterHandlers(app, strict)
+
+	return srv
+}
+
+func (s *Server) Start() {
+	// Channel to listen for errors coming from the listener
+	serverErrors := make(chan error, 1)
+
+	// Start the server
+	go func() {
+		log.Printf("Starting server on %s", s.cfg.ListenAddress)
+
+		serverErrors <- s.app.Listen(s.cfg.ListenAddress)
+	}()
+
+	// Channel to listen for an interrupt or terminate signal from the OS
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+
+	// Blocking main and waiting for shutdown or server errors
+	select {
+	case err := <-serverErrors:
+		log.Fatalf("Error starting server: %v", err)
+
+	case <-shutdown:
+		err := s.app.ShutdownWithTimeout(s.cfg.ShutdownTimeout)
+		if err != nil {
+			log.Printf("Error during server shutdown: %v", err)
+		}
+
+		log.Println("Server gracefully stopped")
 	}
 }
 
